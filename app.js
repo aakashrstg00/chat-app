0d51b38037bc1ae4db09bab45eafc1a5460b760f
Aakash <1998aakashrastogi.com>
2018-01-31 22:59:42 +0530

    t1

--- app.js
+++ app.js
@@ -1,5 +1,58 @@
-const express = require('express');
-const app = express();
-var server = app.listen('4000',()=>{
-    console.log('Started at 4000!');
+const mongo = require('mongodb').MongoClient;
+const client = require('socket.io').listen(4000).sockets;
+
+// mongo connect
+mongo.connect('mongodb://127.0.0.1:27017/mongochat',(err,db)=>{
+    if(err){
+        throw err;
+    }
+    console.log('mongo connected');
+    // connect to socket.io
+    client.on('connection',(socket)=>{
+        let chat = db.collection("chats");
+        // create function to send status
+        sendStatus = function(s){
+            socket.emit('status',s);
+        }
+        // get chats from db
+        chat.find().limit(100).sort({_id:1}).toArray((err,res)=>{
+            if(err) throw err;
+
+            // emit messages
+            socket.emit('output',res);
+        });
+        // handle inout events
+        socket.on('input',data=>{
+            let name = data.name;
+            let message = data.message;
+            // check for name and message
+            if(name && message){
+                // insert message to db
+                chat.insert({
+                    name:name,
+                    message:message
+                },()=>{
+                    client.emit('output',[data]);
+                    //send status
+                    sendStatus({
+                        message: 'Message Sent!',
+                        clear:true
+                    });
+                });
+            }
+            else{
+                // send error status
+                sendStatus('Enter Name and Message both!');
+            }
+        });
+
+        // handle clear
+        socket.on('clear',data=>{
+            // remove all chats from collection
+            chat.remove({},()=>{
+                // emit cleared event
+                socket.emit('cleared!');
+            });
+        });
+    });
 });
